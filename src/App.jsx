6589b8444057d436f44e0b32e916afdcc6490bba
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-06-12 22:17:49 +0200

    fetch api and then display data

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,32 @@ import { useState, useEffect } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [error, setError] = useState(0)
+  const [data, setData] = useState(0)
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response)=> response.json())
+    .then((data)=> setData(data))
+    .catch((error)=>{
+      setError(error);
+    })
+  }, []);
+
 
   return (
+    <>
+    {!error && data
+      ? data.map((post)=> (
+        <div>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))
+      : null}
+      {error ? <h1>{error}</h1> :null}
+    </>
 
-  )
+  ); }
 
 export default App
