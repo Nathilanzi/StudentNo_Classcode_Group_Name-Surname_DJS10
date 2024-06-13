84bc329e9dbc5c2f70b632b0a699591ce86763ae
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-06-13 16:04:36 +0200

    Update useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,41 @@ import { useState, useEffect } from 'react'
 function App() {
   const [error, setError] = useState(0)
   const [data, setData] = useState(0)
+  const [isLoading, setIsLoading]
+
 
   useEffect(()=>{
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response)=> response.json())
-    .then((data)=> setData(data))
-    .catch((error)=>{
-      setError(error);
-    })
-  }, []);
+    const fetchPost = async() => {
+      try{
+        const response = await fetch
+        ("https://jsonplaceholder.typicode.com/posts")
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new error(errorData.message|| " ");
+        }
 
+        const data = await response.json();
+        setPosts(data);
+      }catch (error) {
+        setError(error.message);
+      }finally {
+        setIsLoading(false);
+      }
+    };
+    
+      })
+      fetchPost()
+  }, []);
 
   return (
     <>
+    <div>
+      <h1>Posts</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      )
+      }
+    </div>
     {!error && data
       ? data.map((post)=> (
         <div>
