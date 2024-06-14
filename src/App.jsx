b8a12dbfdb1eb360d2c77a61d1c271cb239094ee
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-06-14 09:50:49 +0200

    fix syntax errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 function App() {
-  const [error, setError] = useState(0)
-  const [data, setData] = useState(0)
-  const [isLoading, setIsLoading]
+  const [error, setError] = useState('');
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
 
   useEffect(()=>{
-    const fetchPost = async() => {
+    const fetchPosts = async() => {
       try{
         const response = await fetch
-        ("https://jsonplaceholder.typicode.com/posts")
+        ("https://jsonplaceholder.typicode.com/posts");
         if (!response.ok) {
           const errorData = await response.json();
-          throw new error(errorData.message|| " ");
+          throw new error(errorData.message|| "An error occured");
         }
 
-        const data = await response.json();
-        setPosts(data);
+        const postsData = await response.json();
+        setPosts(postsData);
       }catch (error) {
         setError(error.message);
       }finally {
@@ -27,30 +26,41 @@ function App() {
       }
     };
     
-      })
-      fetchPost()
+      fetchPosts();
   }, []);
 
   return (
     <>
     <div>
-      <h1>Posts</h1>
+      <h1>post</h1>
       {isLoading ? (
         <p>Loading...</p>
+      ) : error ? (
+        <div style={{color: "black", textAlign: "center"}}>
+          {error}
+        <img
+        src={errorImage}
+        alt="error"
+        style={{ maxWidth: "100%", height: "auto"}}
+        />
+        </div>
+      ) : (
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          {posts.map((post, index) => (
+            <li
+            key={post.id}
+            >
+              <h2>
+                {index + 1}. {post.title}
+              </h2>
+              <p>{post.body}</p>
+            </li>
+          ))}
+        </ul>
       )
       }
     </div>
-    {!error && data
-      ? data.map((post)=> (
-        <div>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </div>
-      ))
-      : null}
-      {error ? <h1>{error}</h1> :null}
-    </>
-
-  ); }
-
+      </>
+  )
+}
 export default App
